bda22e7f618596fa65f23ed17b8bbb7adaa17669
karthikeya1610 <karthikeya@zaktis.com>
2025-10-26 11:30:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Dashboard from "./screens/Dashboard/Dashboard";
+import EmployeeManagement from "./screens/EmployeeManagement/EmployeeManagement";
+function App() {
+  return (
+    <div>
+      {/* <Dashboard /> */}
+      <EmployeeManagement />
+    </div>
+  );
+}
+
+export default App;
