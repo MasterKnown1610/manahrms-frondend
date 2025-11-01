456416d1afbfc29f19f42c58abefd856b95635c2
Your Name <Umesh-219>
2025-11-01 15:50:41 +0530

    Changed Some added a Button design to testing and changed the colors of all

--- src/screens/Testing/Testing.jsx
+++ src/screens/Testing/Testing.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import "./Testing.css"
+import "./Buttons/button_1.css"
+
+
 const Testing = () => {
   return (
     <>
     <div>
-      <h1>Testing</h1>
+      <h1 className="gap">Testing</h1>
     </div>
-    
+
+    <div>
+      <button className="button">Submit</button>
+    </div>
     </>
   );
 };
